1c8caf0dd072bee723cc48e46f9abebea9d6c8fb
Aamir Amin <aamir@Kaizars-MacBook-Air.local>
2023-09-11 10:50:28 +0100

    cookies work

--- Volitile.py
+++ Volitile.py
@@ -1,20 +1,23 @@
 import requests
 
-#appid numbers
+# appid numbers
 
-csgo = '730'
-rust = '252490'
-armello = '290340'
+csgo = "730"
+rust = "252490"
+armello = "290340"
 
-#url = 'https://steamcommunity.com/market/search?appid=' +csgo
+# url = 'https://steamcommunity.com/market/search?appid=' +csgo
 
-cookie = {'steamLogin': '76561198058933558%7C%7C2553658936E891AAD'}
+cookie = {
+    "steamLoginSecure": "76561199003829124%7C%7CeyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInI6MEQxOV8yMzIyQUY3OV85MEFFNiIsICJzdWIiOiAiNzY1NjExOTkwMDM4MjkxMjQiLCAiYXVkIjogWyAid2ViIiBdLCAiZXhwIjogMTY5NDUxMzIwMCwgIm5iZiI6IDE2ODU3ODUxMTYsICJpYXQiOiAxNjk0NDI1MTE2LCAianRpIjogIjBEMjlfMjMyMkFGNzdfODVCOUEiLCAib2F0IjogMTY5NDQyNTA5MiwgInJ0X2V4cCI6IDE3MTI4MDA0NDAsICJwZXIiOiAwLCAiaXBfc3ViamVjdCI6ICIzMS45NC41LjEzOSIsICJpcF9jb25maXJtZXIiOiAiMzEuOTQuNS4xMzkiIH0.LzchuQWdvqOswsn2tGi2gWEW1QrFueeBoUTe60VVeZbSxmMk9cXTMyamPZwoP6SHvpXhy6hpHf141lIKgrHiBQ",
+    "sessionid": "102dd93f2ea34518d538e652",
+}
 
-url = 'http://steamcommunity.com/market/pricehistory/?appid=730&market_hash_name=Glove%20Case%20Key'
+url = "http://steamcommunity.com/market/pricehistory/?appid=730&market_hash_name=Glove%20Case%20Key"
 
 response = requests.get(url, cookies=cookie)
 
-print(response)
+print(response.text)
 
 
-#https://stackoverflow.com/questions/31961868/how-to-retrieve-steam-market-price-history
+# https://stackoverflow.com/questions/31961868/how-to-retrieve-steam-market-price-history
